be8cbf4dba875201ea1b94ab1047e9b3fb402c68
Asep Hidayat <hidayatasep43@gmail.com>
2021-01-01 00:15:10 +0700

    Fix conflict id in image

--- app/src/main/java/id/asep/breedscat/data/model/breeds/Image.kt
+++ app/src/main/java/id/asep/breedscat/data/model/breeds/Image.kt
@@ -1,12 +1,13 @@
 package id.asep.breedscat.data.model.breeds
 
+import androidx.room.ColumnInfo
 import com.google.gson.annotations.SerializedName
 
 
 data class Image (
-
-	@SerializedName("height") val height : Int,
+	@ColumnInfo(name = "imageId")
 	@SerializedName("id") val id : String,
+	@SerializedName("height") val height : Int,
 	@SerializedName("url") val url : String,
 	@SerializedName("width") val width : Int
 )
